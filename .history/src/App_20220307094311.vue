239483870acd89ce84924cb51b9a49c645543d3a
Zero-Qi <1194660649@qq.com>
2022-03-07 11:32:18 +0800

    del

--- .history/src/App_20220307094311.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <DataAnnotations />
-</template>
-<script setup>
-import DataAnnotations from './view/dataAnnotations/index'
-</script>
--- .history/src/App_20220307095131.vue
+++
@@ -1,8 +0,0 @@
-<template>
-<div>
-   dfdsafds
-</div>
-</template>
-<script setup>
-import DataAnnotations from './view/dataAnnotations/index'
-</script>
